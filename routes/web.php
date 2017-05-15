5cbc6ee8baefd5fd083dec05d46fe1da34b05e49
AndresAguilera <andres.aguilera93@gmail.com>
2017-05-15 01:14:01 -0300

    Agregadas funcionalidades básicas
    Chat es capaz de:
    
    Traer y desplegar los mensajes almacenados en la base de datos.
    
    Recibir y guardar un mensaje de un usuario, desplegarlo y guardarlo en
    la base de datos.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,43 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/chat', function () {
+
+    // recibir mensaje del cliente y guardarlo en la bd
+
+    if(isset($_GET['msg'])){
+        $msg = new \App\Mensaje();
+        $msg->usuario = $_GET['user'];
+        $msg->texto = $_GET['msg'];
+        $msg->fecha = \Carbon\Carbon::now();
+        $msg->save();
+    }
+
+    // obtener mensajes de la bd
+
+    $mensajes = App\Mensaje::all();
+
+
+    // paso los datos a la plantilla blade
+
+    return view('chat', ['mensajes' => $mensajes]);
+});
+
+Route::post('/chatpost', function () {
+
+    // recibir mensaje del cliente y guardarlo en la bd
+
+    $msg = new \App\Mensaje();
+    $msg->usuario = $_POST['user'];
+    $msg->texto = $_POST['msg'];
+    $msg->fecha = \Carbon\Carbon::now();
+    $msg->save();
+
+//     obtener mensajes de la bd
+
+    $mensajes = App\Mensaje::all();
+
+//     paso los datos a la plantilla blade
+
+    return view('chat', ['mensajes' => $mensajes]);
+});
